4d63f95c8be86eda3b100b10b741f3721204acc0
dsk-code <dsk0425lucky@iCloud.com>
2025-01-17 11:28:36 +0900

    classesエンドポイントのmodel作成

--- backend/server/src/model/responses/classes.rs
+++ backend/server/src/model/responses/classes.rs
@@ -3,10 +3,11 @@ use ids_shared::{UserId, ClassId};
 use serde::{Deserialize, Serialize};
 use chrono::NaiveDateTime;
 
-
+/// api/v1/classes のレスポンス構造体
+/// api/v1/classes/:class_id のレスポンス構造体
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
-pub struct ClassEntity {
+pub struct ResponseClass {
     pub id: ClassId,
     pub user_id: UserId,
     pub class_name: String,
