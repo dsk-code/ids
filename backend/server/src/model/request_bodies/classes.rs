d514ad8c5c4861b74f53d225c62febcaf28e3e12
dsk-code <dsk0425lucky@iCloud.com>
2025-01-17 11:19:40 +0900

    classesエンドポイントのmodel作成

---
+++ backend/server/src/model/request_bodies/classes.rs
@@ -0,0 +1,39 @@
+use ids_shared::{UserId, ClassId};
+
+use serde::{Deserialize, Serialize};
+
+/// POST api/v1/classes のリクエスト構造体
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct RequestPostClass {
+    pub id: ClassId,
+    pub user_id: UserId,
+    pub class_name: String,
+    pub age: i32,
+}
+
+/// GET api/v1/classes/:class_id のリクエスト構造体
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct RequestGetClass {
+    pub id: ClassId,
+    pub user_id: UserId,
+}
+
+/// PUT api/v1/classes/:class_id のリクエスト構造体
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct RequestUpdateClass {
+    pub id: ClassId,
+    pub user_id: UserId,
+    pub class_name: String,
+    pub age: i32,
+}
+
+/// DELETE api/v1/classes/:class_id のリクエスト構造体
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct RequestDeleteClass {
+    pub id: ClassId,
+    pub user_id: UserId,
+}
